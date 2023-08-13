d4e2c019174a37e76a3383ee501264a719ba7d60
Irakli Svanidze <svanidzeee@gmail.com>
2023-08-13 19:59:41 +0300

    upsert

---
+++ src/app/api/user/upsert/[id]/route.ts
@@ -0,0 +1,28 @@
+import prisma from '@/lib/prisma';
+import { log } from 'console';
+
+interface Body {
+  name: string;
+}
+
+// http://localhost:3000/api/user/upsert
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+  const body: Body = await request.json();
+
+  const updatedUser = await prisma.user.upsert({
+    where: {
+      id: +params.id,
+    },
+
+    // if user exist - update
+    update: {
+      name: 'useExists',
+    },
+    // if not - create
+    create: {
+      name: 'User6',
+      email: 'User6@gmail.com',
+    },
+  });
+  return new Response(JSON.stringify(updatedUser));
+}
